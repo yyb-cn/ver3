ced439de3d929d9066f7dd36839e68907f15b60c
pfcf <888@pfcf.cn>
2015-04-20 09:38:45 +0800

    order_id为关联的订单号ID

--- system/libs/cart.php
+++ system/libs/cart.php
@@ -50,42 +50,29 @@ function payment_paid($payment_notice_id, $outer_notice_sn = '')
 	{
 		$payment_notice = $GLOBALS['db']->getRow("select * from ".DB_PREFIX."payment_notice where id = ".$payment_notice_id);
 		$payment_info = $GLOBALS['db']->getRow("select * from ".DB_PREFIX."payment where id = ".$payment_notice['payment_id']);
-		if($payment_info['class_name'] == 'Voucher')
-		{
-			$GLOBALS['db']->query("update ".DB_PREFIX."deal_order set pay_amount = pay_amount + ".$payment_notice['money'].",ecv_money = ".$payment_notice['money']." where id = ".$payment_notice['order_id']." and is_delete = 0 and order_status = 0 and ((pay_amount + ".$payment_notice['money']." <= total_price) or ".$payment_notice['money'].">=total_price)");
-			$order_incharge_rs = $GLOBALS['db']->affected_rows();
-		}
-		elseif($payment_info['class_name'] == 'Account')
-		{
-			$GLOBALS['db']->query("update ".DB_PREFIX."deal_order set pay_amount = pay_amount + ".$payment_notice['money'].",account_money = account_money + ".$payment_notice['money']." where id = ".$payment_notice['order_id']." and is_delete = 0 and order_status = 0 and pay_amount + ".$payment_notice['money']." <= total_price");
-			$order_incharge_rs = $GLOBALS['db']->affected_rows();
-		}
-		else
-		{
-			$GLOBALS['db']->query("update ".DB_PREFIX."deal_order set pay_amount = pay_amount + ".$payment_notice['money']." where id = ".$payment_notice['order_id']." and is_delete = 0 and order_status = 0 and pay_amount + ".$payment_notice['money']." <= total_price");
-			$order_incharge_rs = $GLOBALS['db']->affected_rows();
-			
-		}
 		$GLOBALS['db']->query("update ".DB_PREFIX."payment set total_amount = total_amount + ".$payment_notice['money']." where class_name = '".$payment_info['class_name']."'");									
 		
+<<<<<<< .mine
 		if(!$order_incharge_rs)
 		{
 
 			//超出充值
+=======
+		$rs = $GLOBALS['db']->affected_rows();	
+		//if (intval($payment_notice['order_id']) == 0){//order_id为关联的订单号ID也就是fanwe_deal_order表的id字段，在v3版本已经去除fanwe_deal_order表。则order_id为0 应该判断 上个操作fanwe_payment表update 是否执行成功。 
+		if ($rs){	//	
+			//充值
+>>>>>>> .theirs
 			require_once APP_ROOT_PATH."system/libs/user.php";
-			$order_info = $GLOBALS['db']->getRow("select is_delete,order_status from ".DB_PREFIX."deal_order where id = ".intval($payment_notice['order_id']));
-			if($order_info['is_delete']==1||$order_info['order_status']==1)
-                        {
-                            $msg = sprintf($GLOBALS['lang']['DELETE_INCHARGE'],$payment_notice['notice_sn']);
-                        }
-                        else
-                        {
-                            $msg = sprintf($GLOBALS['lang']['PAYMENT_INCHARGE'],$payment_notice['notice_sn']);			
-                            modify_account(array('money'=>$payment_notice['money'],'score'=>0),$payment_notice['user_id'],$msg);
-                            //更新订单的extra_status为1
-                            $GLOBALS['db']->query("update ".DB_PREFIX."deal_order set extra_status = 1 where is_delete = 0 and id = ".intval($payment_notice['order_id']));
-                        }
-                }
+			
+			//$msg = sprintf($GLOBALS['lang']['PAYMENT_INCHARGE'],$payment_notice['notice_sn']);	//lang 显示错误
+                        $msg = sprintf($GLOBALS['lang']['MONEYORDER_INCHARGE'],$payment_notice['notice_sn']);	
+			modify_account(array('money'=>$payment_notice['money'],'score'=>0),$payment_notice['user_id'],$msg,1);
+			
+			//在此处开始生成付款的短信及邮件
+			send_payment_sms($payment_notice_id);
+			send_payment_mail($payment_notice_id);
+		}
 		
 		//在此处开始生成付款的短信及邮件
 		send_payment_sms($payment_notice_id);
