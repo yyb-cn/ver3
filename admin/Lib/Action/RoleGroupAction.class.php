d03e71f5484c37b64903cc5e58d7d9cd0e1d1044
li wen long <990371245@qq.com>
2015-05-29 17:17:29 +0800

    增加权限目录管理的添加

--- admin/Lib/Action/RoleGroupAction.class.php
+++ admin/Lib/Action/RoleGroupAction.class.php
@@ -14,6 +14,48 @@ class RoleGroupAction extends CommonAction{
 		$this->assign("default_map",$condition);
 		parent::index();
 	}
+	public function add()
+	{
+		
+		 $nav_list = M("Role_nav")->where("is_effect=1")->findAll();
+		
+		$this->assign("nav_list",$nav_list);
+		parent::add();
+	}
+	public function del()
+	{
+		 if($_REQUEST['id']){
+	  $id=$_REQUEST['id'];
+      $model=M("PfcfVirtualCurrency");
+	  $model->delete($id);
+	  $this->success("删除成功");
+      } 
+		echo 123;exit;
+	}
+	public function insert()
+	{
+		$data['name']=$_REQUEST['name'];
+		$data['nav_id']=$_REQUEST['nav_id'];
+        $data['is_delete ']=0;
+		$data['is_effect']=$_REQUEST['is_effect'];
+		$data['sort']=$_REQUEST['sort'];
+		
+		$role_id=M('role_group')->add($data);
+		
+		if($role_id){
+			
+			
+		    save_log($log_info.L("INSERT_SUCCESS"),1);
+			$this->success(L("INSERT_SUCCESS"));
+			
+		}else {
+			//错误提示
+			save_log($log_info.L("INSERT_FAILED"),0);
+			$this->error(L("INSERT_FAILED"));
+		}
+		
+	
+	}
 	
 }
 ?>
