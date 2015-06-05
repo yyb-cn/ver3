f6f7e1e0236b6f625d8070c84fb5d407d3633342
995450048 <995450048@qq.com>
2015-06-05 16:39:01 +0800

    字体修改

--- public/db_config.php
+++ public/db_config.php
@@ -1,9 +1,9 @@
 <?php
 return array(
 'DB_HOST'=>'localhost',
-'DB_NAME'=>'zandao',
+'DB_NAME'=>'v3',
 'DB_USER'=>'root',
-'DB_PWD'=>'',
+'DB_PWD'=>'123456',
 'DB_PORT'=>'3306',
 'DB_PREFIX'=>'fanwe_',
 );
